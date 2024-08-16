6f118ab27fde4d1bdb794689c5f82f0eb84f58d4
owujib <owujibfavour@gmail.com>
2024-08-16 16:36:10 +0100

    added links

--- app/(client)/test.tsx
+++ app/(client)/test.tsx
@@ -2,11 +2,17 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import { ThemedView } from '@/components/ThemedView';
 import { ThemedText } from '@/components/ThemedText';
+import { Link } from 'expo-router';
 
 export default function Test() {
   return (
     <ThemedView className="h-full">
       <ThemedText>Test Page</ThemedText>
+      <View className="w-1/2 h-30 bg-blue-700 rounded-md items-center">
+        <Link href="/(tabs)/" className="text-xl text-white my-4 ">
+          Go to tabs screen
+        </Link>
+      </View>
     </ThemedView>
   );
 }
